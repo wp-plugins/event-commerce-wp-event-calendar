55f3e9d3ca9abfbae4096e01533ea71bc3b56345
wpproducts <wpproducts@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-12 08:05:00 +0000

    pro version link updated
    git-svn-id: https://plugins.svn.wordpress.org/event-commerce-wp-event-calendar/trunk@1110922 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/event_pro_version.php
+++ includes/event_pro_version.php
@@ -12,13 +12,13 @@
 <div class="wrapper">
   <div class="wrap" style="float:left; width:100%;">
     <div id="icon-options-general" class="icon32"></div>
-    <div style="width:70%;float:left;"><h2>Wordpress Event Commerce Pro</h2></div>
+    <div style="width:70%;float:left;"><h2>EventCommerce WP Responsive Event Calendar Pro</h2></div>
        <div class="main_div">
         <div class="metabox-holder" style="width:98%; float:left;">
             <div id="namediv" class="stuffbox" style="width:60%;min-height:450px;">
               <h3 class="top_bar">More Features Available in Our Pro Version:-</h3>
               <div style="padding-top:15px;margin-left:20px;">
-                <a class="btnprover" target="_blank" href="http://wordpress-expert.codeinterest.com/wordpress/eventcommerce-wp-event-calendar-pro" style="width:100px;height: 20px;background-color: green;color:white;" >EventCommerce WP Event Calendar Pro</a>
+                <a class="btnprover" target="_blank" href="http://codecanyon.net/item/eventcommerce-wp-responsive-event-calendar-pro/10716644?ref=wpproducts" style="width:100px;height: 20px;background-color: green;color:white;" >EventCommerce WP Responsive Event Calendar Pro</a>
               </div>  
                <form id="frmCssFix" action="" method="post" style="width:100%">
                  <div style="margin: 20px;">
@@ -42,7 +42,7 @@
                     </ol>
                    <div style="padding-top:15px;">
                      <b>Please Have Our Professional Version to Increase your Business </b><br/><br/><br/>
-                     <a class="btnprover" target="_blank" href="http://wordpress-expert.codeinterest.com/wordpress/eventcommerce-wp-event-calendar-pro" style="width:100px;height: 20px;background-color: green;color:white;" >EventCommerce WP Event Calendar Pro</a>
+                     <a class="btnprover" target="_blank" href="http://codecanyon.net/item/eventcommerce-wp-responsive-event-calendar-pro/10716644?ref=wpproducts" style="width:100px;height: 20px;background-color: green;color:white;" >EventCommerce WP Responsive Event Calendar Pro</a>
                     </div>
                    <div style="float:right;padding-top:35px; font-size: 11px;">Developed by: <a target="_blank" href="http://www.solvercircle.com">SolverCircle Ltd</a></div>
                  </div> 
